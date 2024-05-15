f625d6d42450fd7461ae393303041576cc84ffef
kidmagus <msamson19@Outlook.com>
2024-05-15 22:17:41 +0800

    changes :>

--- js/profile.js
+++ js/profile.js
@@ -4,3 +4,15 @@ loadFile.addEventListener("change", (event) => {
   const image = document.querySelector(".output");
   image.src = URL.createObjectURL(event.target.files[0]);
 });
+
+const box = document.querySelectorAll(".box");
+
+box.forEach((box) => {
+  const boxHead = box.querySelector(".head");
+
+  if (boxHead) {
+    boxHead.addEventListener("click", () => {
+      box.classList.toggle("expanded");
+    });
+  }
+});
