8ed62493650ba3658cb0c14f4ad5eef199344906
Matthew Samson <msamson19@Outlook.com>
2024-05-21 23:12:23 +0800

    Update splide.js

--- js/splide.js
+++ js/splide.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-	new Splide(".splide", {
-		type: "loop",
-		perPage: 3,
-		autoplay: true,
-	}).mount();
-});
+const splide = new Splide(".splide", { type: "loop", focus: "center" }).mount();
