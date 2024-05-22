b2b340c4546a5ba75b350337df860338068d4da2
kidmagus <msamson19@Outlook.com>
2024-05-22 17:56:11 +0800

    sticky header with splide.js

---
+++ js/general.js
@@ -0,0 +1,21 @@
+const header = document.querySelector(".header");
+const nav = document.querySelector(".nav");
+const headerHeight = header.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${headerHeight}px`,
+});
+
+headerObserver.observe(header);
--- js/splide.js
+++ js/splide.js
@@ -1,1 +1,20 @@
-const splide = new Splide(".splide", { type: "loop", focus: "center" }).mount();
+const splide = new Splide(".splide", {
+  focus: "center",
+  type: "fade",
+  rewind: "true",
+  perPage: "1",
+  autoplay: true,
+  interval: 3000,
+}).mount();
+
+document.addEventListener("DOMContentLoaded", function () {
+  splide.on("active", function (e) {
+    const element = document.querySelectorAll(".splide__content");
+    element[e.index].classList.add("animate");
+  });
+
+  splide.on("inactive", function (e) {
+    const element = document.querySelectorAll(".splide__content");
+    element[e.index].classList.remove("animate");
+  });
+});
