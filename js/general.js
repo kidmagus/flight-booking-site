498ea2bfefc79261e23839e95cf1561dae588273
kidmagus <msamson19@Outlook.com>
2024-05-22 23:45:13 +0800

    apply intersecting section--hidden

--- js/general.js
+++ js/general.js
@@ -1,6 +1,6 @@
 const header = document.querySelector(".header");
 
-const section1 = document.getElementById("section1");
+const trigger = document.getElementById("trigger");
 const stickyNav = function (entries) {
   const [entry] = entries;
   console.log(entry);
@@ -13,6 +13,6 @@ const stickyNav = function (entries) {
 };
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
-  threshold: 0.6,
+  threshold: 0.2,
 });
-headerObserver.observe(section1);
+headerObserver.observe(trigger);
